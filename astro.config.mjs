cb277353d7444789bdecd5c752f600d6e54941f9
sbeas <cheviotin200@gmail.com>
2025-02-11 23:46:42 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://SebastianMoralesDuque.github.io",
-  base: "esoterismo",
+  site: "https://misionerodelamor.site",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
